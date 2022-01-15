2e8781ff4c24a364309b058ea9cabb1f1472b3c0
john-rizikianos <80098482+john-rizikianos@users.noreply.github.com>
2022-01-15 22:29:27 +0200

    Add files via upload

---
+++ omada48/src/main/java/gr/hua/omada48/HelloController.java
@@ -0,0 +1,43 @@
+package gr.hua.working_app;
+
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @Autowired private SecurityUserDetailsService userDetailsManager;
+    @Autowired private PasswordEncoder passwordEncoder;
+
+    @PostMapping(
+            value = "/register",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = {
+            MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }
+    )
+    public void addUser(@RequestParam Map<String, String> body) {
+        Pog user = new Pog(); user.setUsername(body.get("username"));
+        user.setPassword(passwordEncoder.encode(body.get("password")));
+        userDetailsManager.createUser(user);
+    }
+    private String getErrorMessage(HttpServletRequest request, String key) {
+        Exception exception = (Exception) request.getSession().getAttribute(key);
+        String error = "";
+        if (exception instanceof BadCredentialsException) {
+            error = "Invalid username and password!";
+        } else if (exception instanceof LockedException) {
+            error = exception.getMessage();
+        } else {
+            error = "Invalid username and password!";
+        }
+        return error;
+    }
+}
---
+++ omada48/src/test/java/gr/hua/working_app/WorkingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.hua.working_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
